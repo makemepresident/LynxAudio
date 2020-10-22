715044c1ba91bce8926d9eeff8cc1be7e5a69f5c
Derik <ddreher@unbc.ca>
2020-10-21 22:15:23 -0700

    adds general handling for audio post

--- api/src/js/index.js
+++ api/src/js/index.js
@@ -1,37 +1,48 @@
 const exp = require('express')
 const app = exp()
-const dbep = 'lynxdb-1.cbqydpcjfuag.us-east-2.rds.amazonaws.com'
 const port = 8080
-const {Pool, Client} = require('pg')
+const {Client} = require('pg') // Destructuring - equivalent to saying const tt = require('pg') and tt.Client
 // const port = 5432
 const log = console.log
+app.use(exp.json())
 
-// Post voicememo data
-app.post('/postmemo', (req, res) => {
-    // check headers
-    // check authentication
-    // make insert request to db
+function construct_client() {
     let client = new Client({
         user: 'postgres',
-        host: dbep,
+        host: 'lynxdb-1.cbqydpcjfuag.us-east-2.rds.amazonaws.com',
         database: 'lynxdb',
         password: 'Yz7S6nF2h6fgACN',
         port: 5432})
+    return client
+}
 
+function db_insertAudioClip(input) {
+    let client = construct_client()
     client.connect()
-
+    let values = []
+    values[0] = input.id
+    values[1] = input.userid
+    values[2] = input.audiopath
+    values[3] = input.cliplength
+    values[4] = input.filesize
     let text = 'INSERT INTO audioclips(id, userid, audiopath, cliplength, filesize) VALUES($1, $2, $3, $4, $5)'
-    let values = [1, 2, 'testing', 1, 1]
     client.query(text, values, (err, res) => {
-                    if(err) {
-                        log('unable to post')
-                        log(err)
-                    } else {
-                        log('success')
-                    }
-                client.end()
-                })
+        if(err) {
+            log('Query unsuccessful')
+            log(err)
+            return
+        }
+        log('Success')
+        client.end()
+    })
+}
+
+app.post('/postmemo', (req, res) => {
+    db_insertAudioClip(req.body);
+    res.send('Success')
 })
+
+// Post voicememo 
 
 // Post login
 

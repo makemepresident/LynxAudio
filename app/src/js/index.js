699ba7b5876098711da5aba59b2c4b690c8ac829
Derik <62659313+makemepresident@users.noreply.github.com>
2020-11-10 18:28:45 -0800

    Modified folder structure, fixed express routing such that audiorecorder is correctly served, tested and had blob returned

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,16 +1,22 @@
 const exp = require('express')
+const path = require('path')
 const app = exp()
 const api_host = "PLACEHOLDER -- will be localhost of some sort (same EC2 instance)"
 const port = 8080
 const log = console.log
 
+log(path.join(__dirname, '../public'))
+app.use(exp.static(path.join(__dirname, '../public')))
+
 // Routing
-app.get('/', (req, res) => {
-    res.send('main page sending request')
-    // responsible for serving index.html (entry function)
-})
-
-
+// app.get('/', (req, res) => {
+//     // responsible for serving index.html (entry function)
+//     try {
+//         res.sendFile('/index.html')
+//     } catch(err) {
+//         log(err)
+//     }
+// })
 
 app.listen(port, () => {
     log("Webapp is running")

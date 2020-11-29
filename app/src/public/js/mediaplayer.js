8c76b25b2d45d7c49b3721bf11d5e8770bfaea81
Shotz5 <kitsulalex@gmail.com>
2020-11-28 20:13:53 -0800

    working autio button

--- app/src/public/js/mediaplayer.js
+++ app/src/public/js/mediaplayer.js
@@ -1,3 +1,5 @@
+
+// Main controller
 let playpause = document.getElementById("playpausepath")
 let currenttime = document.getElementById("currenttime")
 let totaltime = document.getElementById("totaltime")
@@ -9,9 +11,60 @@ let loading = document.getElementById("loading")
 let playpausevis = document.getElementById("playpausevis")
 let slider = document.getElementById("slider")
 let volumebutton = document.getElementById("volumebutton")
-let volumecontainer = document.getElementById("volumecontainer")
+
+// Volume controller
+let volumecontroller = document.getElementById("volumecontainer")
+let volumeslider = document.getElementById("volslider")
+let volumeprogress = document.getElementById("volprogress")
+let volumedot = document.getElementById("voldot")
 
 let volumeshown = false
+
+// Volume button handlers
+
+volumebutton.onclick = () => {
+    if (!volumeshown) {
+        volumeshown = true;
+        volumecontainer.style = "display: visible"
+    } else {
+        volumeshown = false;
+        volumecontainer.style = "display: none"
+    }
+}
+
+volumedot.onmousedown = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+
+    let x = event.clientX
+    let min = 0
+    let max = parseInt(window.getComputedStyle(volumeslider).width)
+    let current = parseInt(window.getComputedStyle(volumeprogress).width)
+
+    window.onmousemove = (event2) => {
+        event2 = event2 || window.event;
+        event2.preventDefault();
+            
+        let calculatedwidth = (event2.clientX - x) + parseInt(current)
+        if (calculatedwidth >= min && calculatedwidth < max) {
+            volumeprogress.style = "width: " + ((event2.clientX - x) + current) + "px"
+            let ratio = (calculatedwidth / max)
+            audiocontroller.volume = ratio.toFixed(2)
+        }
+    }
+}
+
+// Audio controller handers
+
+playpause.onclick = () => {
+    if (audiocontroller.paused == true) {
+        playpause.setAttribute("d", "M 0 0 L 0 24 L 6 24 L 6 0 M 18 0 L 18 24 L 12 24 L 12 0")
+        audiocontroller.play()
+    } else {
+        playpause.setAttribute("d", "M 18 12 L 0 24 V 0 0")
+        audiocontroller.pause()
+    }
+}
 
 audiocontroller.onloadedmetadata = () => {
     loading.style = "display: none"
@@ -37,7 +90,7 @@ audiocontroller.onloadedmetadata = () => {
             event2.preventDefault();
             
             let calculatedwidth = (event2.clientX - x) + parseInt(current)
-            if (calculatedwidth >= 0 && calculatedwidth < max) {
+            if (calculatedwidth >= min && calculatedwidth <= max) {
                 progress.style = "width: " + ((event2.clientX - x) + current) + "px"
 
                 let ratio = (calculatedwidth / max)
@@ -58,26 +111,6 @@ audiocontroller.onended = () => {
 audiocontroller.ontimeupdate = () => {
     currenttime.innerHTML = audiocontroller.currentTime.toFixed(2)
     progress.style.width = ((audiocontroller.currentTime / audiocontroller.duration) * 100).toFixed(2) + "%"
-}
-
-playpause.onclick = () => {
-    if (audiocontroller.paused == true) {
-        playpause.setAttribute("d", "M 0 0 L 0 24 L 6 24 L 6 0 M 18 0 L 18 24 L 12 24 L 12 0")
-        audiocontroller.play()
-    } else {
-        playpause.setAttribute("d", "M 18 12 L 0 24 V 0 0")
-        audiocontroller.pause()
-    }
-}
-
-volumebutton.onclick = () => {
-    if (!volumeshown) {
-        volumeshown = true;
-        volumecontainer.style = "display: visible"
-    } else {
-        volumeshown = false;
-        volumecontainer.style = "display: none"
-    }
 }
 
 audiosource.src = "../uploads/3b7e7251bfec10f7751fb574188f7b65"
